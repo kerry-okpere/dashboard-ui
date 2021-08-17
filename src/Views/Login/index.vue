dd3b9df1848c2beaa1214d0e3ddc5f7ffaaae4f1
kerry-okpere <kerryokpere@gmail.com>
2021-08-17 19:59:22 +0100

    add bg image

--- src/Views/Login/index.vue
+++ src/Views/Login/index.vue
@@ -1,34 +1,35 @@
 <template>
-  <section class="h-screen flex items-center justify-center">
-    <div>
-      <img src="../../assets/img/vector.svg" alt="logo" class=" sm:pb-12"/>
+  <section class="h-screen w-full bg relative sm:border-l sm:border-r sm:border-dashed">
+    <div class="fixed inset-0 flex items-center justify-center">
+      <div>
+        <img src="../../assets/img/vector.svg" alt="logo" class=" sm:pb-12"/>
+        <form @submit.prevent="handleSubmit"  class="sm:shadow-secondary sm:bg-white sm:py-12 sm:px-16 rounded">
+          <div class="sm:w-96">
+            <h1 class="text-gray-800 text-2xl font-semibold py-4">Sign in to your account</h1>
+            <div class="py-4">
+              <Input v-model="user.name" label="Email" type="email"/>
+            </div>
+            <div class="py-4">
+              <Input v-model="user.password" label="Password" type="password" alt-label="Forgot your password?"/>
+            </div>
+            
+            <Checkbox class="py-4" v-model="user.agree">
+              <p class="font-normal text-sm">Stay signed in for a week</p>
+            </Checkbox>
 
-      <form @submit.prevent="handleSubmit"  class="sm:shadow-secondary sm:py-12 sm:px-16 rounded">
-        <div class="sm:w-96">
-          <h1 class="text-gray-800 text-2xl font-semibold py-4">Sign in to your account</h1>
-          <div class="py-4">
-            <Input v-model="user.name" label="Email" type="email"/>
+            <Button class="my-4 w-full" @click="$router.push('/payments')">Continue</Button>
+
+            <Link title="Use single sign-on (SSO) instead" class="block text-center py-4" />
           </div>
-          <div class="py-4">
-            <Input v-model="user.password" label="Password" type="password" alt-label="Forgot your password?"/>
-          </div>
-          
-          <Checkbox class="py-4" v-model="user.agree">
-            <p class="font-normal text-sm">Stay signed in for a week</p>
-          </Checkbox>
+        </form>
 
-          <Button class="my-4 w-full" @click="$router.push('/payments')">Continue</Button>
-
-          <Link title="Use single sign-on (SSO) instead" class="block text-center py-4" />
-        </div>
-      </form>
-
-      <p class="text-sm text-gray-700 py-4">Don’t have an account? <Link title="Sign up" /></p>
-      <ul class="list-disc flex w-full space-x-7 text-sm text-gray-600 py-2">
-        <li class="list-none">© Stripe</li>
-        <li>Contact</li>
-        <li>Privacy &amp; terms</li>
-      </ul>
+        <p class="text-sm text-gray-700 py-4">Don’t have an account? <Link title="Sign up" /></p>
+        <ul class="list-disc flex w-full space-x-7 text-sm text-gray-600 py-2">
+          <li class="list-none">© Stripe</li>
+          <li>Contact</li>
+          <li>Privacy &amp; terms</li>
+        </ul>
+      </div>
     </div>
   </section>
 </template>
@@ -65,3 +66,9 @@ export default defineComponent({
   },
 })
 </script>
+<style lang="scss" scoped>
+.bg{
+  background-image: url('../../assets/img/grey-bg.svg');
+  background-repeat: no-repeat;
+}
+</style>
