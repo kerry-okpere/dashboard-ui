14781ab052105299168df4fb38b85c2aba66e1e3
kerry-okpere <kerryokpere@gmail.com>
2021-08-17 18:01:59 +0100

    add random routes

--- src/router/index.js
+++ src/router/index.js
@@ -2,15 +2,55 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 const routes = [
   {
-    path: '/',
-    name: 'Dashboard',
-    component: () => import('../Views/Dashboard/index.vue'),
-  },
-  {
     path: '/login',
     name: 'Login',
     component: () => import('../Views/Login/index.vue'),
-  }
+  },
+  {
+    path: '/',
+    name: 'Home',
+    component: () => import('../Views/Dashboard/index.vue'),
+  },
+  {
+    path: '/payments',
+    name: 'Payment',
+    component: () => import('../Views/Dashboard/index.vue'),
+  },
+  {
+    path: '/balances',
+    name: 'Balances',
+    component: () => import('../Views/Dashboard/index.vue'),
+  },
+  {
+    path: '/customers',
+    name: 'Customers',
+    component: () => import('../Views/Dashboard/index.vue'),
+  },
+  {
+    path: '/accounts',
+    name: 'Accounts',
+    component: () => import('../Views/Dashboard/index.vue'),
+  },
+  {
+    path: '/products',
+    name: 'Products',
+    component: () => import('../Views/Dashboard/index.vue'),
+  },
+  {
+    path: '/reports',
+    name: 'Reports',
+    component: () => import('../Views/Dashboard/index.vue'),
+  },
+  {
+    path: '/developers',
+    name: 'Developers',
+    component: () => import('../Views/Dashboard/index.vue'),
+  },
+  {
+    path: '/settings',
+    name: 'Settings',
+    component: () => import('../Views/Dashboard/index.vue'),
+  },
 ]
 
 const router = createRouter({
