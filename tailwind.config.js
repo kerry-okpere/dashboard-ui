3961a8ce1cedc55c6bfadfe41ee3efe69124bde4
kerry-okpere <kerryokpere@gmail.com>
2021-08-17 18:00:40 +0100

    manage taikwind styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,23 +6,27 @@ module.exports = {
       colors: {
         primary: '#635CFF',
         secondary: '#5469D4',
+        tertiary: '#7DABF8',
+        'gray-10': '#C2C7CF',
+        'gray-20': '#E3E8EE',
+        'gray-30': '#D9DCE1',
         'gray-40': '#F7FAFC',
+        'gray-600': '#697386',
         'green-60': '#CBF4C9',
         'green-70': '#0E6245'
+      },
+      width: {
+        '9/20': '45%',
+        '11/20': '55%'
+      },
+      margin: {
+        '5.5': '22px'
       }
     },
     boxShadow: {
-      sm: '0 1px 2px 0 rgba(0, 0, 0, 0.05)',
-      DEFAULT: '0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06)',
-      md: '0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -1px rgba(0, 0, 0, 0.06)',
-      lg: '0 10px 15px -3px rgba(0, 0, 0, 0.1), 0 4px 6px -2px rgba(0, 0, 0, 0.05)',
-      xl: '0 20px 25px -5px rgba(0, 0, 0, 0.1), 0 10px 10px -5px rgba(0, 0, 0, 0.04)',
-      '2xl': '0 25px 50px -12px rgba(0, 0, 0, 0.25)',
-      '3xl': '0 35px 60px -15px rgba(0, 0, 0, 0.3)',
-      inner: 'inset 0 2px 4px 0 rgba(0, 0, 0, 0.06)',
-      none: 'none',
       primary: '0px 2px 5px rgba(60, 66, 87, 0.08), 0px 0px 0px 1px rgba(60, 66, 87, 0.16), 0px 1px 1px rgba(0, 0, 0, 0.12)',
-      secondary: '0px 5px 15px rgba(0, 0, 0, 0.12), 0px 15px 35px rgba(60, 66, 87, 0.08);'
+      secondary: '0px 5px 15px rgba(0, 0, 0, 0.12), 0px 15px 35px rgba(60, 66, 87, 0.08);',
+      tertiary: '0px 1px 1px rgba(0, 0, 0, 0.12), 0px 2px 5px rgba(60, 66, 87, 0.08);'
     },
     fontSize: {
       'xxs': '.6888rem',
@@ -30,15 +34,8 @@ module.exports = {
       'xsm': '.814rem',
       'sm': '.875rem',
       'base': '1rem',
-      'lg': '1.125rem',
-      'xl': '1.25rem',
       '2xl': '1.5rem',
       '2.5xl': '1.75rem',
-      '3xl': '1.875rem',
-      '4xl': '2.25rem',
-      '5xl': '3rem',
-      '6xl': '4rem',
-      '7xl': '5rem',
      }
   },
   variants: {
