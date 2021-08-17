761e0b0df1c3df740afd2274bb2daa458a2df5f4
kerry-okpere <kerryokpere@gmail.com>
2021-08-17 19:28:02 +0100

    options component

---
+++ src/components/Options/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="relative">
+    <DotsHorizontalIcon class="h-4 w-4" @click="open = !open"/>
+    <ul class="absolute shadow-secondary py-1.5 bg-white z-10 w-24 right-0 rounded-md" v-if="open">
+      <li class="text-sm text-gray-600 py-1 px-2 hover:bg-gray-20 transition-colors cursor-pointer">Delete</li>
+    </ul>
+  </div>
+</template>
+<script>
+import { defineComponent, ref } from 'vue'
+import { DotsHorizontalIcon } from '@heroicons/vue/solid';
+
+export default defineComponent({
+  components: {
+    DotsHorizontalIcon
+  },
+  setup() {
+    const open = ref(false)
+    return {
+      open
+    }
+  },
+})
+</script>
--- src/components/Table/index.vue
+++ src/components/Table/index.vue
@@ -25,7 +25,7 @@
           <div class="truncate w-40">{{data.customer}}</div>
         </td>
         <td class="text-sm font-normal">{{data.date}}</td>
-        <td><DotsHorizontalIcon class="h-4 w-4"/></td>
+        <td><Options /></td>
       </tr>
     </tbody>
   </table>
@@ -48,14 +48,14 @@ import { defineComponent, watch, ref } from 'vue'
 import Checkbox from '@/components/Checkbox/index.vue'
 import Badge from '@/components/Badge/index.vue'
 import Button from '@/components/Button/index.vue'
-import { DotsHorizontalIcon } from '@heroicons/vue/solid';
+import Options from '@/components/Options/index.vue'
 
 export default defineComponent({
   components: {
     Checkbox,
     Badge,
     Button,
-    DotsHorizontalIcon
+    Options
   },
   props: {
     dataTable: {
