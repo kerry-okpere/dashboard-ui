e2ffd02c0e009eb87693faa39871d3844b354183
kerry-okpere <kerryokpere@gmail.com>
2021-08-17 18:01:04 +0100

    create menu

---
+++ src/components/Menu/group.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="py-2">
+    <slot />
+  </div>
+</template>
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  setup() {
+    
+  },
+})
+</script>
---
+++ src/components/Menu/item.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="text-sm py-1">
+    <router-link :to="route" @click="open = !open"
+      :class="[' flex items-center', 
+        $route.path == route ? 'text-secondary font-semibold' : 'text-gray-700']">
+      <span class="mr-5.5">
+        <slot/>
+      </span>
+      {{title}} 
+    </router-link>
+    <ul v-if="open && links?.length" class="pt-1 pb-3">
+      <li v-for="(link, index) in links" :key="index" class="ml-10 py-0.5 font-normal text-gray-700 cursor-pointer">{{link}}</li>
+    </ul>
+  </div>
+</template>
+<script>
+import { defineComponent, ref } from 'vue'
+
+export default defineComponent({
+  props: {
+    title: {
+      type: String,
+      required: true
+    },
+    route: {
+      type: String,
+      default: '/'
+    },
+    links: {
+      type: Array,
+    }
+  },
+  setup() {
+    const open = ref(true)
+    return {
+      open
+    }
+  },
+})
+</script>
